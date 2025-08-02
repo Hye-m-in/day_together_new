4a32150ccf88e27c4497c1293d068654b2f717a4
sujin <gusrjs1203@gmail.com>
2025-08-02 14:19:27 +0900

    local changes before merge with main

--- python_app/app/models/schemas.py
+++ python_app/app/models/schemas.py
@@ -1,11 +1,11 @@
 # python_app/app/schemas.py
 from pydantic import BaseModel
+from pydantic import Field
 
 class GoogleTokenRequest(BaseModel):
     id_token: str
 
 class NaverTokenRequest(BaseModel):
-    access_token: str
-
+    access_token: str = Field(..., alias="access_token")
 class TokenResponse(BaseModel):
     custom_token: str
